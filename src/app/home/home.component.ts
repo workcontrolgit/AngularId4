b497815edd460df0b4fd74c8cd3288d1bc37ad6d
fuji.nguyen <fuji.nguyen@workcontrol.com>
2021-01-31 13:23:48 -0500

    remote quotes.service.ts.  format test api table

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { finalize } from 'rxjs/operators';
 
-import { QuoteService } from './quote.service';
 import { DataService } from './data.service';
 import { Logger } from '@core';
 import { PersonQuery } from '@app/models/person.query';
@@ -18,22 +17,10 @@ export class HomeComponent implements OnInit {
   isLoading = false;
   webapiData$: PersonQuery;
 
-  constructor(private quoteService: QuoteService, private dataService: DataService) {}
+  constructor(private dataService: DataService) {}
 
   ngOnInit() {
     log.debug('init');
-
-    this.isLoading = true;
-    this.quoteService
-      .getRandomQuote({ category: 'dev' })
-      .pipe(
-        finalize(() => {
-          this.isLoading = false;
-        })
-      )
-      .subscribe((quote: string) => {
-        this.quote = quote;
-      });
   }
   callapi() {
     this.isLoading = true;
